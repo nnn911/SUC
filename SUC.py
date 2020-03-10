83c53dba45bceaa8f5bd5749cfe99c99ee79b750
Daniel Utt <utt@mm.tu-darmstadt.de>
2020-03-10 09:44:52 +0100

    Initial commit

---
+++ SUC.py
@@ -0,0 +1,20 @@
+
+def J2eV(val):
+    return val/e
+
+
+def eV2J(val):
+    return val*e
+
+
+# Boltzman constant
+k = 1.380649e-23
+
+# Elementary charge
+e = 1.602176634e-19
+
+# Avogadro's number:
+NA = 6.02214076e23
+
+# Boltzman constant in eV
+k_eV = J2eV(k)
